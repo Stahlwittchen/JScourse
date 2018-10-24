5e0ed698700d7eaaef071f5ec2eb3dfcd671f014
Melita_Eletckaia <Melita_Eletckaia@epam.com>
2018-10-24 19:00:15 +0300

    Created template for booking form

--- src/public/scripts/order-form.js
+++ src/public/scripts/order-form.js
@@ -1,1 +1,24 @@
-console.log('check');
+function order() {
+    const parent = document.getElementById('order'),
+          button = document.getElementById('sendOrder');
+    let orderData = {},
+        auth;
+
+    parent.onclick = function(e) {
+        if(e.target != parent){
+            orderData.date = new Date().toLocaleDateString();
+            orderData.content = e.target.innerHTML;
+            orderData.user = auth || 'anonim';
+        }
+        console.log(orderData);
+        return orderData;
+    };
+
+    button.onclick = function(e) {
+        if (Object.keys(orderData).length ){
+            const data = orderData;
+            console.log(data);
+        }
+    }
+}
+setTimeout(order, 0);
