1409523422e2a7783dd6313a25b4e224a4e99fe1
Melita_Eletckaia <melita_eletckaia@epam.com>
2018-11-01 18:38:22 +0300

    Refactored with ejs

--- src/app.js
+++ src/app.js
@@ -1,18 +1,17 @@
 import express from 'express';
-import es6Renderer from 'express-es6-template-engine';
 const app = express();
 
 import home from './routes/home'
 import recipe from './routes/recipe'
 import booking from './routes/booking'
 
-app.engine('html', es6Renderer);
+app.set('view engine', 'ejs');
+
 app.set('views', __dirname + '/templates');
-app.set('view engine', 'html');
 app.use(express.static(__dirname + '/public'));
 
-app.use('/', home);
-app.use('/recipes', recipe);
+// app.use('/', home);
+// app.use('/recipes', recipe);
 app.use('/booking', booking);
 
 module.exports = app;
--- src/routes/booking.js
+++ src/routes/booking.js
@@ -1,14 +1,11 @@
 import express from 'express';
 const router = express.Router();
-
+import classes from '../public/data/master-classes';
 router.get('/', function (req, res) {
     res
         .status(200)
         .render('booking',{
-            partials: {
-                pageHead: '/page-head',
-                nav: '/nav'
-            }
+            list: classes
         })
 });
 
