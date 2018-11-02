0cfa522e8f3ac7f08397714cdc5d75c60caded1b
Melita_Eletckaia <melita_eletckaia@epam.com>
2018-11-02 18:24:26 +0300

    Refactored order form

--- src/routes/booking.js
+++ src/routes/booking.js
@@ -2,13 +2,14 @@ import express from 'express';
 const router = express.Router();
 import classes from '../public/data/master-classes';
 
-router.get('/', function (req, res) {
-    res
-        .status(200)
-        .render('booking',{
-            list: classes,
-            menuID: 'booking'
+router
+    .get('/', function (req, res) {
+        res
+            .status(200)
+            .render('booking',{
+                list: classes,
+                menuID: 'booking'
+            })
         })
-});
 
 module.exports = router;
