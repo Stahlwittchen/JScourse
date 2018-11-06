387add4f4d3033c5f96eabbcafc0987166acbd4e
Melita_Eletckaia <melita_eletckaia@epam.com>
2018-11-06 18:50:55 +0300

    Updated recipe page: data and template

--- src/routes/recipe.js
+++ src/routes/recipe.js
@@ -1,17 +1,19 @@
 import express from 'express';
 const router = express.Router();
 import recipes from '../data/recipes';
-import _ from "underscore";
 
 router.get('/:title', function (req, res) {
-    const title = _.find(recipes, {title: req.params.title});
-    if (title === undefined ) {
-        res.status(404)
-            .json({message: `${req.params.title} not found`})
+    let currentRecipe;
+
+    for(var i=0; i<recipes.length; i++){
+        if(recipes[i].title==req.params.title){
+            currentRecipe = recipes[i];
+            break;
+        }
     }
+
     res.render('recipe',{
-        title: req.params.title,
-        desc: title.desc,
+        currentRecipe: currentRecipe,
         menuID: 'recipe',
         user: req.session.username
     })
